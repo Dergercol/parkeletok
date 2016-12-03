45f8a6fdff77f2f35897a06b0831f29257bcc243
Vladislav Smirnov <Vladislav Smirnov>
2016-12-03 10:38:08 +0500

    Spaces

--- auth/login.php
+++ auth/login.php
@@ -1,6 +1,6 @@
 <?php
 
-  function __autoload( $className ) {
+function __autoload( $className ) {
     $className = str_replace( "..", "", $className );
     require_once( "classes/$className.php" );
     echo "Loaded classes/$className.php<br>";
