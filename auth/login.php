647ac9a16dd598afa52853115ff5a6a4943d2061
Vladislav Smirnov <beluckydaf@gmail.com>
2016-12-03 13:49:46 +0500

    Added some spaces for testing purposes

--- auth/login.php
+++ auth/login.php
@@ -1,10 +1,11 @@
 <?php
 
-    	function __autoload( $className ) {
-  $className = str_replace( "..", "", $className );
-  require_once( "classes/$className.php" );
-  echo "Loaded classes/$className.php<br>";
+  function __autoload( $className ) {
+    $className = str_replace( "..", "", $className );
+    require_once( "classes/$className.php" );
+    echo "Loaded classes/$className.php<br>";
 }
+
 $member = new Member();
 echo "Создан обьект: ";
 
